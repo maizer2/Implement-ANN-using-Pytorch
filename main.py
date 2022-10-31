22670628f1b519803a9a66e7052ad91d2743fcd4
maizer2 <jhw3979@gmail.com>
2022-10-31 14:30:26 +0000

    add

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from network.GAN.dcgan import Generator, Discriminator
+from network.GAN.dcgan import Generator, Discriminator, weights_init
 
 import torch
+import torch.nn as nn
 import torch.utils.data as data
+import torch.optim as optim
 
 import torchvision
 import torchvision.transforms as transforms
@@ -11,10 +13,25 @@ from tqdm import tqdm
 
 # --------------------------------------------------------------- #
 
-batch_size = 1
-num_workers = 4
 
 if __name__ == "__main__":
+
+    ##################
+    # Hyperparameter #
+    ##################
+
+    num_gpus = 3
+    batch_size = 128
+    num_workers = 4
+    num_epochs = 20000
+    device = torch.device("cuda" if torch.cuda.is_available() and num_gpus > 0 else "cpu")
+    latent_space_vector = 100
+    lr = 0.0002
+    betas = (0.5, 0.999)
+
+    ###########
+    # Prepare #
+    ###########
 
     train_data = datasets.MNIST(
         root="/data/DataSet/",
@@ -34,8 +51,48 @@ if __name__ == "__main__":
 
     )
 
-    num_epochs = 20000
+    netG = Generator().to(device)
+    netD = Discriminator().to(device)
+
+    if (device.type == "cuda") and (num_gpus > 1):
+        netG = nn.DataParallel(netG, list(range(num_gpus)))
+        netD = nn.DataParallel(netD, list(range(num_gpus)))
+
+    netG.apply(weights_init)
+    netD.apply(weights_init)
+
+    criterion = nn.BCELoss()
+    optimizerG = optim.Adam(
+        params=netG.parameters(),
+        lr=lr,
+        betas=betas
+    )
+    optimizerD = optim.Adam(
+        params=netD.parameters(),
+        lr=lr,
+        betas=betas
+    )
+    #########
+    # Train #
+    #########
 
     for epochs in tqdm(range(1, num_epochs + 1)):
         for idx, (imgs, _) in enumerate(train_loader):
+            netG.zero_grad(), netD.zero_grad()
+
+            x = imgs.to(device)
+            y = torch.ones((x.size(0), ), dtype=torch.float, device=device)
+            noise = torch.randn((x.size(0), latent_space_vector, 1, 1), dtype=torch.float, device=device)
+
+            ##########################
+            # Training Discriminator #
+            ##########################
+
+            y_hat = netD(x).view(-1)
+            loss = criterion(y_hat, y)
             
+            print(loss)
+
+            
+            break
+        break
--- network/GAN/dcgan.py
+++ network/GAN/dcgan.py
@@ -6,6 +6,16 @@ Using MNIST 1x28x28 DataSet
 
 # --------------------------------------------------------------- #
 
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm") != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+# --------------------------------------------------------------- #
+
 class Generator(nn.Module):
     def __init__(self):
         super().__init__()
