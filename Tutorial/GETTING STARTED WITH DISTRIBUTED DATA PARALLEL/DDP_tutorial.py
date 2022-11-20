9b1a1f4f21ee158b08b55790fcbc833d5953ec82
maizer2 <jhw3979@gmail.com>
2022-11-21 00:00:24 +0900

    add

--- Tutorial/GETTING STARTED WITH DISTRIBUTED DATA PARALLEL/DDP_tutorial.py
+++ Tutorial/GETTING STARTED WITH DISTRIBUTED DATA PARALLEL/DDP_tutorial.py
@@ -9,12 +9,85 @@ import torch.multiprocessing as mp
 
 from torch.nn.parallel import DistributedDataParallel as DDP
 
-def setup(rank, workl_size):
+def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
 
-    dist.init_process_group("nccl", rank=rank, world_size=workl_size)
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
 def cleanup():
     dist.destroy_process_group()
 
+class ToyModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.layer = nn.Sequential(
+            nn.Linear(10, 10),
+            nn.ReLU(),
+            nn.Linear(10, 5)
+        )
+    
+    def forward(self, x):
+        return self.layer(x)
+
+def demo_basic(rank, world_size):
+    print(f"Running basic DDP example on rank {rank}.")
+    setup(rank, world_size)
+
+    model = ToyModel.to(rank)
+    ddp_model = DDP(model, device_ids=[rank])
+
+    loss_fn = nn.MSELoss()
+    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+
+    optimizer.zero_grad()
+    outputs = ddp_model(torch.randn(20, 10))
+    labels = torch.randn(20, 5).to(rank)
+    loss_fn(outputs, labels).backward()
+    optimizer.step()
+
+    cleanup()
+
+def run_demo(demo_fn, world_size):
+    mp.spawn(
+        demo_fn,
+        args=(world_size,),
+        nprocs=world_size,
+        join=True
+    )
+
+def demo_checkpoint(rank, world_size):
+    print(f"Running DDP checkpoint example on rank : {rank}.")
+    setup(rank, world_size)
+
+    model = ToyModel().to(rank)
+    ddp_model = DDP(model, device_ids=[rank])
+
+    CHECKPOINT_PATH = tempfile.gettempdir() + "/model.checkpoint"
+    if rank == 0:
+        torch.save(ddp_model.state_dict(), CHECKPOINT_PATH)
+    
+    dist.barrier()
+
+    map_location = {'cuda:%d' % 0: 'cuda:%d' % rank}
+    ddp_model.load_state_dict(
+        torch.load(CHECKPOINT_PATH, map_location=map_location)
+    )
+
+    loss_fn = nn.MSELoss()
+    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+
+    optimizer.zero_grad()
+    outputs = ddp_model(torch.randn(20, 10))
+    labels = torch.randn(20, 5).to(rank)
+
+    loss_fn(outputs, labels).backward()
+    optimizer.step()
+
+    if rank == 0:
+        os.remove(CHECKPOINT_PATH)
+
+    cleanup()
+
+def 
